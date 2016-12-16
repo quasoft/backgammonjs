c022f7dca202e6e3d5e478f90dc55e55aca31a59
QuaSoft <info@quasoft.net>
2016-12-16 21:12:14 +0200

    Add rule selector on landing page

--- app/browser/js/config.js
+++ app/browser/js/config.js
@@ -3,5 +3,9 @@ var config = {};
 config.containerID = 'backgammon';
 config.boardUI = '../app/browser/js/SimpleBoardUI.js';
 config.defaultRule = 'RuleBgCasual';
+config.selectableRules = [
+  'RuleBgCasual',
+  'RuleBgGulbara'
+];
 
 module.exports = config;
--- app/browser/js/main.js
+++ app/browser/js/main.js
@@ -8,8 +8,13 @@ var bootstrap = require('bootstrap');
 var clipboard = require('clipboard');
 var cl = require('../../../lib/client');
 var comm = require('../../../lib/comm.js');
+var model = require('../../../lib/model.js');
+require('../../../lib/rules/rule.js');
+require('../../../lib/rules/RuleBgCasual.js');
+require('../../../lib/rules/RuleBgGulbara.js');
 
 function App() {
+  this._config = {};
   this._isWaiting = false;
   this._currentView = 'index';
   
@@ -39,7 +44,104 @@ function App() {
       $('#game-view').show();
     }
   }
-}
+  
+  /**
+   * Get name of rule selected by player
+   * @returns {string} - Name of selected rule.
+   */
+  this.getSelectedRuleName = function () {
+    var selected = $('#rule-selector label.active input').val();
+    return selected;
+  };
+  
+  /**
+   * Load rule module
+   * @param {string} ruleName - Rule's name, equal to rule's class name (eg. RuleBgCasual)
+   * @returns {Rule} - Corresponding rule object
+   */
+  this.loadRule = function (rulePath, ruleName) {
+    var fileName = model.Utils.sanitizeName(ruleName);
+    var file = rulePath + fileName + '.js';
+    var rule = require(file);
+    rule.name = fileName;
+    return rule;
+  };
+  
+  /**
+   * Initialize selector of game rule
+   */
+  this.initRuleSelector = function () {
+    // Init rule selector
+    selector = $('#rule-selector');
+    var i;
+    for (i = 0; i < this._config.selectableRules.length; i++) {
+      var ruleName = this._config.selectableRules[i];
+      var rule = app.loadRule('../../../lib/rules/', ruleName);
+      var isSelected = (i == 0);
+      var isActive = isSelected ? 'active' : '';
+      var isChecked = isSelected ? 'checked' : '';
+
+      var item = $('#tmpl-rule-selector-item').html();
+      item = item.replace('{{name}}', rule.name);
+      item = item.replace('{{title}}', rule.title);
+      item = item.replace('{{active}}', isActive);
+      item = item.replace('{{checked}}', isChecked);
+
+      selector.append($(item));
+    }
+  };
+  
+  /**
+   * Initialize application. Must be called after DOM is ready.
+   * @param {Object} config - Configuration object
+   */
+  this.init = function (config) {
+    var self = this;
+    this._config = config;
+    
+    this.initRuleSelector();
+    
+    // Initialize the overlay showing game results
+    $('#game-result-overlay').click(function () {
+      $('#game-result-overlay').hide();
+    });
+
+    // Initialize game client
+    var client = new cl.Client(config);
+
+    // Subscribe to events used on landing page
+    client.subscribe(comm.Message.EVENT_MATCH_START, function (msg, params) {
+      self.setIsWaiting(false);
+      self.setCurrentView('game');
+      self.updateView();
+      client.resizeUI();
+    });
+
+    client.subscribe(comm.Message.EVENT_MATCH_OVER, function (msg, params) {
+      self.setIsWaiting(false);
+      self.setCurrentView('index');
+      self.updateView();
+    });
+
+    $('#btn-create-match').click(function (e) {
+      client.reqCreateMatch(config.defaultRule);
+    });
+
+    $('#btn-join-match').click(function (e) {
+      client.reqJoinMatch(null);
+    });
+
+    $('#btn-play-random').click(function (e) {
+      self.setIsWaiting(true);
+      self.updateView();
+      client.reqPlayRandom(self.getSelectedRuleName());
+    });
+
+    $(window).resize(function () {
+      client.resizeUI();
+    });
+  };
+};
 
 var app = new App();
 
@@ -50,44 +152,5 @@ $(document).ready(function() {
   // Prepare client config
   var config = require('./config');
   
-  // Initialize the overlay showing game results
-  $('#game-result-overlay').click(function () {
-    $('#game-result-overlay').hide();
-  });
-  
-  // Initialize game client
-  var client = new cl.Client(config);
-  
-  // Subscribe to events used on landing page
-  client.subscribe(comm.Message.EVENT_MATCH_START, function (msg, params) {
-    app.setIsWaiting(false);
-    app.setCurrentView('game');
-    app.updateView();
-    client.resizeUI();
-  });
-  
-  client.subscribe(comm.Message.EVENT_MATCH_OVER, function (msg, params) {
-    app.setIsWaiting(false);
-    app.setCurrentView('index');
-    app.updateView();
-  });
-
-  $('#btn-create-match').click(function (e) {
-    client.reqCreateMatch(config.defaultRule);
-  });
-
-  $('#btn-join-match').click(function (e) {
-    client.reqJoinMatch(null);
-  });
-  
-  $('#btn-play-random').click(function (e) {
-    app.setIsWaiting(true);
-    app.updateView();
-    client.reqPlayRandom(config.defaultRule);
-  });
-  
-  $(window).resize(function () {
-    client.resizeUI();
-  });
-
+  app.init(config);
 });
